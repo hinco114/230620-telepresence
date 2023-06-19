16120cba1c6da61966b5676d8227bcbcd13b8f9d
SangHyoun Yun <hainco@gmail.com>
2023-06-20 00:19:23 +0900

    Add elasticache

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,25 @@ resource "aws_elasticache_subnet_group" "subnet_group" {
   subnet_ids = module.vpc.private_subnet
 }
 
+data "http" "my_ip" {
+  url = "http://whatismyip.akamai.com/"
+}
+
+resource "aws_security_group" "allow_my_ip" {
+  name        = "allow_my_ip"
+  description = "Allow inbound traffic from my IP"
+  vpc_id      = module.vpc.vpc
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "${data.http.my_ip.response_body}/32",
+    ]
+  }
+}
+
 resource "aws_elasticache_cluster" "demo_cluster" {
   cluster_id           = "elasticache-cluster-demo"
   engine               = "redis"
@@ -18,6 +37,7 @@ resource "aws_elasticache_cluster" "demo_cluster" {
   engine_version       = "3.2.10"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.subnet_group.name
+  security_group_ids   = [aws_security_group.allow_my_ip.id]
 }
 
 output "cache_address" {
